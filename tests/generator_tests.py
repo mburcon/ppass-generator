b61b07f2f6c5f82fc203bd0b621d0d4cdd7173ce
Mateusz Burcon <mburcon@sii.pl>
2022-12-29 13:41:40 +0100

    add tests

---
+++ tests/generator_tests.py
@@ -0,0 +1,43 @@
+import pytest
+import string
+from new_pass.generator import PasswordGenerator
+
+def test_generate():
+    generator = PasswordGenerator()
+    password = generator.generate()
+    
+    # Test that the generated password is the correct length
+    assert len(password) == 16
+    
+    # Test that the generated password only contains ASCII letters and digits
+    assert all(c in string.ascii_letters + string.digits for c in password)
+
+def test_generate_length():
+    generator = PasswordGenerator(length=32)
+    password = generator.generate()
+    
+    # Test that the generated password is the correct length
+    assert len(password) == 32
+    
+    # Test that the generated password only contains ASCII letters and digits
+    assert all(c in string.ascii_letters + string.digits for c in password)
+
+def test_generate_special_chars():
+    generator = PasswordGenerator(special_chars=True)
+    password = generator.generate()
+    
+    # Test that the generated password is the correct length
+    assert len(password) == 16
+    
+    # Test that the generated password contains ASCII letters, digits, and special characters
+    assert all(c in string.ascii_letters + string.digits + string.punctuation for c in password)
+
+def test_generate_length_special_chars():
+    generator = PasswordGenerator(length=32, special_chars=True)
+    password = generator.generate()
+    
+    # Test that the generated password is the correct length
+    assert len(password) == 32
+    
+    # Test that the generated password contains ASCII letters, digits, and special characters
+    assert all(c in string.ascii_letters + string.digits + string.punctuation for c in password)
