4fd992c9a96af9a26ca7cd9ff48d00e43992f83c
Mateusz Burcon <mburcon@sii.pl>
2022-12-29 13:40:28 +0100

    add library code

---
+++ src/new_pass/generator.py
@@ -0,0 +1,43 @@
+import argparse
+import secrets
+import string
+
+class PasswordGenerator:
+    def __init__(self, length=16, special_chars=False):
+        """Initialize the password generator with the specified options.
+        
+        Args:
+            length (int, optional): The length of the generated password (default: 16).
+            special_chars (bool, optional): Whether to include special characters in the generated password (default: False).
+        """
+        self.length = length
+        self.special_chars = special_chars
+    
+    def generate(self):
+        """Generate a password."""
+        # Determine the character set to use
+        if self.special_chars:
+            charset = string.ascii_letters + string.digits + string.punctuation
+        else:
+            charset = string.ascii_letters + string.digits
+        
+        # Generate the password
+        password = "".join(secrets.choice(charset) for i in range(self.length))
+        return password
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Generate a password.")
+    parser.add_argument("-l", "--length", type=int, default=16, help="The length of the generated password (default: 16).")
+    parser.add_argument("-s", "--special-chars", action="store_true", help="Whether to include special characters in the generated password.")
+    args = parser.parse_args()
+    
+    # Generate the password
+    generator = PasswordGenerator(length=args.length, special_chars=args.special_chars)
+    password = generator.generate()
+    
+    # Print the password
+    print(password)
+
+if __name__ == "__main__":
+    main()
